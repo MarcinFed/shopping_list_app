bd5773eaaeb2186986d19a8d716c0dab107fb6bd
MarcinFed <fedorowicz.marcin02@gmail.com>
2024-08-12 21:20:27 +0200

    Added dismiss option for items in a list

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -27,8 +27,44 @@ class _GroceryListState extends State<GroceryList> {
     }
   }
 
+  void _removeItem(GroceryItem item) {
+    setState(() {
+      _groceryItems.remove(item);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget content = const Center(child: Text('No items added yet.'),);
+
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (ctx, index) =>
+            Dismissible(
+              onDismissed: (direction) {
+                _removeItem(_groceryItems[index]);
+              },
+              key: ValueKey(_groceryItems[index].id),
+              child: ListTile(
+                title: Text(
+                  _groceryItems[index].name,
+                ),
+                leading: Container(
+                    width: 24,
+                    height: 24,
+                    decoration: BoxDecoration(
+                      color: _groceryItems[index].category.color,
+                      shape: BoxShape.circle,
+                    )),
+                trailing: Text(
+                  _groceryItems[index].quantity.toString(),
+                ),
+              ),
+            ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Grocery List'),
@@ -39,24 +75,7 @@ class _GroceryListState extends State<GroceryList> {
           ),
         ],
       ),
-      body: ListView.builder(
-        itemCount: _groceryItems.length,
-        itemBuilder: (ctx, index) => ListTile(
-          title: Text(
-            _groceryItems[index].name,
-          ),
-          leading: Container(
-              width: 24,
-              height: 24,
-              decoration: BoxDecoration(
-                color: _groceryItems[index].category.color,
-                shape: BoxShape.circle,
-              )),
-          trailing: Text(
-            _groceryItems[index].quantity.toString(),
-          ),
-        ),
-      ),
+      body: content,
     );
   }
 }
